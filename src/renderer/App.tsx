5ee6d6d0cc0a19b93fcb50f6f051fde2711f8369
Amit Verma <akv004@gmail.com>
2022-08-23 17:38:03 -0500

    Add files via upload

---
+++ src/renderer/App.tsx
@@ -0,0 +1,40 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+import {Page} from "./component/Page";
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://www.electronjs.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Test me OOO
+          </button>
+        </a>
+        <Page/>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/renderer/component/Page.tsx
@@ -0,0 +1,13 @@
+import base64 from "base-64";
+
+export const Page=()=>{
+
+  const name = base64.encode('Test');
+  return(
+    <>
+      <div>{name}</div>
+      <div>{base64.decode(name)}</div>
+    </>
+
+  )
+}
