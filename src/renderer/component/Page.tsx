dbc5e463ad63bedb10f66c4b871c2dfbcd9008d4
averm156 <amit_k_verma@optum.com>
2022-11-22 09:32:52 -0600

    .erb

--- src/renderer/component/Page.tsx
+++ src/renderer/component/Page.tsx
@@ -1,12 +1,23 @@
-import base64 from "base-64";
+//import base64 from "base-64";
+
+// export const Page=()=>{
+//
+//     const name = base64.encode('Test');
+//     return(
+//         <>
+//             <div>{name}</div>
+//             <div>{base64.decode(name)}</div>
+//         </>
+//
+//     )
+// }
 
 export const Page=()=>{
 
-  const name = base64.encode('Test');
+  //const name = base64.encode('Test');
   return(
     <>
-      <div>{name}</div>
-      <div>{base64.decode(name)}</div>
+    <h1>Hello</h1>
     </>
 
   )
