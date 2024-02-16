06364c53ea014a484baa86a6598cdb647e8d58b6
Verma, Amit K <amit_k_verma@optum.com>
2024-02-16 11:36:06 -0600

    nedb

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,7 +14,16 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
-import { DataSource } from 'nedb';
+// @ts-ignore
+import { DataStore } from 'nedb';
+
+
+// Initialize NeDB database
+// @ts-ignore
+const db = new DataSource({
+  filename: path.join(app.getPath('userData'), 'db.db'),
+  autoload: true
+});
 
 class AppUpdater {
   constructor() {
@@ -145,26 +154,27 @@ const createWindow = async () => {
 //   });
 // };
 
-/**
- * Add event listeners...
- */
+  /**
+   * Add event listeners...
+   */
 
-app.on('window-all-closed', () => {
-  // Respect the OSX convention of having the application in memory even
-  // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  app.on('window-all-closed', () => {
+    // Respect the OSX convention of having the application in memory even
+    // after all windows have been closed
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 
-app
-  .whenReady()
-  .then(() => {
-    createWindow();
-    app.on('activate', () => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (mainWindow === null) createWindow();
-    });
-  })
-  .catch(console.log);
+  app
+    .whenReady()
+    .then(() => {
+      createWindow();
+      app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (mainWindow === null) createWindow();
+      });
+    })
+    .catch(console.log);
+}
